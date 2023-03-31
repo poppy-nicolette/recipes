5f7196ebd5187750144854111d82b48d3bd82d7e
poppy-nicolette <pnriddle@dal.ca>
2023-03-31 10:37:33 -0300

    updated recipes

--- fuzzymatch.py
+++ fuzzymatch.py
@@ -3,55 +3,82 @@ from fuzzywuzzy import process
 import pandas as pd
 from cleanup import cleanup
 
-
-# function to pass in cleaned_list
+#function to pass in cleaned_list
 def fuzzymatch(x):
-    # import food items into a list to extract only food items
-    df2 = pd.read_csv("food_list.csv")
 
+    #import food items into a list to extract only food items
+    df2 = pd.read_csv("food_list.csv")
+    
+    #import nonfood_item_list.csv
+    df_nonfood = pd.read_csv("nonfood_list.csv")
+    list3 = df_nonfood["nonFoodItems"].tolist()
+    
     # convert to list...why? just following the example so far...
     list1 = x
     list2 = df2["Food Type"].tolist()
 
-    # cleanup list2 which is our imported food_thesaurus
+    #cleanup list2 which is our imported food_thesaurus
     list2 = cleanup(list2)
 
     # empty lists for storing the matches later
     mat1 = []
     mat2 = []
 
-    # iterating through list1 to extract
-    # it's closest match from list2
+
+    # fuzzymatch from list2
+    mat1 = []
     for i in list1:
-        mat1.append(process.extract(i, list2, limit=2))
-
+        mat1.append(process.extract(i, list2, limit=1))
+    
     df1 = pd.DataFrame(list1)
-    df1.rename(columns={0: 'item'}, inplace=True)
+    df1.rename(columns = {0:'item'}, inplace = True)
     df1['matches'] = mat1
+    
+    #fuzzymatch for nonfoods
+    mat3 = []
+    for i in list1:
+        mat3.append(process.extract(i, list3, limit=1))
+    df1['nonfoods'] = mat3
 
-    # iterating through the closest
-    # matches to filter out the
-    # maximum closest match
-    # taking the threshold as 80
+    #keep only items above threshold in matches
+    #threshold can be adjusted to values between 0 and 100
     threshold = 90
 
     for j in df1['matches']:
         p = []
         for k in j:
             if k[1] >= threshold:
-                p.append(k[0])
-
+                p.append(k[0])       
         mat2.append(",".join(p))
 
-    # storing the resultant matches
-    # back to dframe1
     df1['matches'] = mat2
+    
+    #keep only nonfood items above threshold
+    #filter for nonfood items
+    threshold2 = 90
+    mat3=[]
+    for j in df1['nonfoods']:
+        p = []
+        for k in j:
+            if k[1] >= threshold2:
+                p.append(k[0])
+            else:
+                pass
+        mat3.append(",".join(p))
 
+    df1['nonfoods'] = mat3
+    #remove empty matches
     df1 = df1.replace("", "NaN")
     df3 = df1[df1.filter(['matches']).matches.str.contains('NaN') == False]
+    
+    #remove nonfood_list items from df3
+    df3 = df3.loc[df3["nonfoods"] == "NaN"]
+    df3 = df3.drop('nonfoods', axis=1)
+    
+    #reset index just to make it tidy
     df3.reset_index(drop=True, inplace=True)
 
-    # add system date
+    #add system date
     date = pd.Timestamp.today().strftime('%Y-%m-%d')
     df3 = df3.assign(date=date)
     return df3
--- receipt_reader.py
+++ receipt_reader.py
@@ -1,11 +1,16 @@
+"""
+reference for pytesseract
+https://pypi.org/project/pytesseract/
+It may be necessary to specify the tesseract executable on your local path
+"""
+#!pip install pytesseract
+
 import os
 import pytesseract as ts
 from PIL import Image, ImageFilter
 
-
 def ocr():
     #find all image files in list, jpg or png
-    # try except error by pass
     path_of_the_directory = os.getcwd()
     ext = ('.jpg', '.png')
     image_list = []
@@ -13,7 +18,6 @@ def ocr():
         if files.endswith(ext):
             print("Processing:  " + files)
             image_list.append(files)
-
         else:
             continue
 
@@ -24,13 +28,12 @@ def ocr():
 
         #convert image to string
         text=ts.image_to_string(img)
-        #print(text)
         return text
-        # part str at \n
-        #text_list = text.split('\n')
-        text_list=text
+
     return text_list
     print(text_list)
 
-  #straight from the image as a list of items
   # the list approach keeps the order and relevant tokens together
+
+
+
